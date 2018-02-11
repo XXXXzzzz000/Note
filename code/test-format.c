f7a11dcf74d2a332ade66909cec7e0af06d5168a
XuanZe <xuanze119@gmail.com>
2018-02-11 14:55:55 +0800

    添加测试文件

---
+++ code/test-format.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  printf("%s \n", "hello!");
+  int firefox = 0 for (j = 0; j < 10; j++)
+  {
+    printf("hello\n", );
+  }
+
+  firefox = 1;
+
+  return 0;
+}
